299a3032ab21303005c8e5bbbcd69a169e04fce9
Aleksandr-Kovshar <aleksandr.mpro@gmail.com>
2022-08-24 23:29:16 +0300

    doneTask10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesContainer= document.querySelector('#boxes');
+const input = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+const boxes = [];
+amount = input.value;
+
+for (let i = 1; i<=amount; i+=1){
+  const box = document.createElement('div');
+  box.style.width = `${30+i*10}px`;
+  box.style.height = `${30+i*10}px`;
+  box.style.backgroundColor = getRandomHexColor();
+
+  boxes.push(box);
+}
+console.log(boxes);
+boxesContainer.append(...boxes);
+input.value='';
+}
+
+
+function destroyBoxes(){
+  boxesContainer.innerHTML = '';
+}
